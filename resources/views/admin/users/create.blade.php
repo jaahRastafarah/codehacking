954ad39adebdb23ca51358b50953ef0307ee256f
Jamiu Lawal <jamiulawal9557@gmail.com>
2019-12-07 13:55:49 +0100

    users create page done

--- resources/views/admin/users/create.blade.php
+++ resources/views/admin/users/create.blade.php
@@ -3,7 +3,7 @@
 @section('content')
     <h1>Create Users</h1>
 
-    {!! Form::open(['method'=>'POST', 'action'=>'AdminUsersController@store']) !!}
+    {!! Form::open(['method'=>'POST', 'action'=>'AdminUsersController@store', 'files'=>true]) !!}
 
 
         <div class="form-group">
@@ -16,11 +16,16 @@
     </div>
     <div class="form-group">
         {!! Form::label('role_id', 'Role:') !!}
-        {!! Form::select('role_id',[''=>'Choose Options'] + $roles, null,['class'=>'form-control']) !!}
+        {!! Form::select('role_id',[''=>'Choose Options'] + $roles, null, ['class'=>'form-control']) !!}
     </div>
     <div class="form-group">
-        {!! Form::label('status', 'Status:') !!}
-        {!! Form::select('status',array(1=>'Active', 0 =>'Not Active'), 0,['class'=>'form-control']) !!}
+        {!! Form::label('is_active', 'Status:') !!}
+        {!! Form::select('is_active',array(1=>'Active', 0 =>'Not Active'), 0,['class'=>'form-control']) !!}
+    </div>
+
+    <div class="form-group">
+        {!! Form::label('file', 'File:') !!}
+        {!! Form::file('file',null,['class'=>'form-control']) !!}
     </div>
 
     <div class="form-group">
@@ -34,6 +39,8 @@
       </div>
       {!! Form::close() !!}
 
+
+   @include('includes.error')
 
 
     @stop
